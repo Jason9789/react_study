4f27588220b23bcfd90a89aeb949cd99adf584db
PanGeun <lifebook0809@gmail.com>
2020-01-28 17:38:20 +0900

    study ref Dom

--- ref_dom/src/App.js
+++ ref_dom/src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import ValidationSample from "./ValidationSample";
+// import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
-function App() {
-  return <ValidationSample />;
+class App extends Component {
+  render() {
+    return (
+      //  <ValidationSample />),
+      // <ScrollBox />
+      <div>
+        <ScrollBox ref={ref => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToTop()}>맨 위로</button>
+        <br />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
