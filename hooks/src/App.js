06c30b02fc2ece205a6fec532535dd09b0907029
PanGeun <lifebook0809@gmail.com>
2020-01-29 16:19:13 +0900

    study useEffect

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-import Counter from "./Counter";
+// import Counter from "./Counter";
 import Info from "./Info";
 
 function App() {
+  const [visible, setVisible] = useState(false);
   // return <Counter />;
-  return <Info />;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      {visible && <Info />}
+    </div>
+  );
 }
 
 export default App;
--- hooks/src/Info.js
+++ hooks/src/Info.js
@@ -1,8 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+
+  //   useEffect(() => {
+  //     console.log("렌더링이 완료되었습니다!");
+  //     console.log({
+  //       name,
+  //       nickname
+  //     });
+  //   });
+
+  // 특정 값이 업데이트될 때만 실행하고 싶을 때
+  //   useEffect(() => {
+  //     console.log(name);
+  //   }, [name]);
+
+  // 뒷정리하기
+  useEffect(() => {
+    console.log("effect");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
+  });
 
   const onChangeName = e => {
     setName(e.target.value);
