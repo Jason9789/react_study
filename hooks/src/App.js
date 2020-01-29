66e3dc4d4fede73464a19b01b1e9c9e2022ffb92
PanGeun <lifebook0809@gmail.com>
2020-01-29 16:33:16 +0900

    study useReducer

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -5,20 +5,23 @@ import "./App.css";
 import Info from "./Info";
 
 function App() {
-  const [visible, setVisible] = useState(false);
+  // ========== useEffect 사용 ===============
+  // const [visible, setVisible] = useState(false);
+  // // return <Counter />;
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? "숨기기" : "보이기"}
+  //     </button>
+  //     {visible && <Info />}
+  //   </div>
+  // );
   // return <Counter />;
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      {visible && <Info />}
-    </div>
-  );
+  return <Info />;
 }
 
 export default App;
--- hooks/src/Info.js
+++ hooks/src/Info.js
@@ -1,8 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: ""
+  });
+  const { name, nickname } = state;
+  const onChange = e => {
+    dispatch(e.target);
+  };
+
+  // const [name, setName] = useState("");
+  // const [nickname, setNickname] = useState("");
 
   //   useEffect(() => {
   //     console.log("렌더링이 완료되었습니다!");
@@ -18,28 +34,30 @@ const Info = () => {
   //   }, [name]);
 
   // 뒷정리하기
-  useEffect(() => {
-    console.log("effect");
-    console.log(name);
-    return () => {
-      console.log("cleanup");
-      console.log(name);
-    };
-  });
+  // useEffect(() => {
+  //   console.log("effect");
+  //   console.log(name);
+  //   return () => {
+  //     console.log("cleanup");
+  //     console.log(name);
+  //   };
+  // });
 
-  const onChangeName = e => {
-    setName(e.target.value);
-  };
+  // const onChangeName = e => {
+  //   setName(e.target.value);
+  // };
 
-  const onChangeNickname = e => {
-    setNickname(e.target.value);
-  };
+  // const onChangeNickname = e => {
+  //   setNickname(e.target.value);
+  // };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        {/* <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} /> */}
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
