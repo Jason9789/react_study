36eef16e706eb0995497572a086a1e2f82204af1
PanGeun <lifebook0809@gmail.com>
2020-01-29 15:31:58 +0900

    study useState

---
+++ hooks/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+import Counter from "./Counter";
+import Info from "./Info";
+
+function App() {
+  // return <Counter />;
+  return <Info />;
+}
+
+export default App;
