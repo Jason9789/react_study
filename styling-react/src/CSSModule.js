a5ab43733b4d9f4ae763968392f480c2ded77dcd
PanGeun <lifebook0809@gmail.com>
2020-02-01 23:14:25 +0900

    module.css

---
+++ styling-react/src/CSSModule.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./CSSModule.module.css";
+
+const CSSModule = () => {
+  return (
+    <div className={`${styles.wrapper} ${styles.inverted}`}>
+      안녕하세요, 저는 <span className="something">CSS Module!</span>
+      <br />
+      테스트 <span className="something">테스트</span>
+    </div>
+  );
+};
+
+export default CSSModule;
