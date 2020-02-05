f76f72223288d4d203ebdc62602982d36eef56c6
PanGeun <lifebook0809@gmail.com>
2020-02-05 22:29:10 +0900

    add News Viewer UI

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,33 +1,38 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
+// import axios from 'axios';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
+
+// const App = () => {
+//   const [data, setData] = useState(null);
+//   const onClick = async () => {
+//     try {
+//       const response = await axios.get(
+//         'https://newsapi.org/v2/top-headlines?country=kr&apiKey=9d53eaef21a1426f86e09acfd21bd799',
+//       );
+//       setData(response.data);
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       {data && (
+//         <textarea
+//           rows={7}
+//           value={JSON.stringify(data, null, 2)}
+//           readOnly={true}
+//         />
+//       )}
+//     </div>
+//   );
+// };
 
 export default App;
